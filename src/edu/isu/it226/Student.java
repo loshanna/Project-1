f42fc6bde76e167bb26e3d1bdfd34d225743e74e
O'Shanna, Luke <lroshan@ilstu.edu>
2017-10-04 01:03:49 -0500

    First Commit

---
+++ src/edu/isu/it226/Student.java
@@ -0,0 +1,23 @@
+/*
+ * File name: Student.java
+*
+* Programmer: Luke O'Shanna
+* ULID: LROSHAN
+*
+* Date: Oct 3, 2017
+*
+* Class: IT 179
+*/
+package edu.isu.it226;
+
+/**
+ *<insert class description here>
+ *
+ * @author Luke
+ *
+ */
+public class Student
+{
+	
+	
+}
